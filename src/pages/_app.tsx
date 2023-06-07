3c42f7b409e10d11e36085dec3a7625bd19abe81
Cristian <tradolcesalatoinfo@gmail.com>
2023-06-07 12:28:23 +0200

    aggiustato _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,6 @@ import Layout from '@/Components/Layout/Layout'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Router, useRouter } from 'next/router'
-import AuthContextProvider from './Auth/AuthContext'
-import ProtectedRoute from './Auth/ProtectedRoute'
 
 const noAuthRequired = ['/login',]
 
@@ -12,21 +10,6 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <>
-  {/*<AuthContextProvider>
-    {noAuthRequired.includes(router.pathname) ? (
-      <>
-      <Layout authorized={false}>
-        <Component {...pageProps} />
-      </Layout>
-      </>
-    ) : (
-      <ProtectedRoute>
-          <Layout authorized={true}>
-            <Component {...pageProps} />
-          </Layout>
-        </ProtectedRoute>
-    ) }
-    </AuthContextProvider>*/}
     <Layout>
       <Component {...pageProps} />
     </Layout>
